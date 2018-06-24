fecff815f478129de7918ccaefaa9dabaac1586b
Peter McCready <petermccready2@gmail.com>
2018-06-24 17:27:03 +0100

    Remove comments

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -127,16 +127,9 @@ const LineChart = (container, title, subtitle, yAxis, series, maxWidth = 500) =>
         label: {
           connectorAllowed: false
         },
-        // pointStart: pointStart
       }
     },
     series: series,
-    // series: [
-    //   {
-    //     name: 'Installation',
-    //     data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-    //   },
-    // ],
     responsive: {
       rules: [{
         condition: {
@@ -176,7 +169,6 @@ const lineCharts = [
         color: 'blue',
       },
     ],
-    // pointStart: 'Jan',
   }
 ]
 
@@ -197,72 +189,6 @@ const createLineChart = (chart, index, parentId) => {
   const { title, subtitle, yAxis, series } = chart;
   return LineChart(container, title, subtitle, yAxis, series)
 }
-
-// const createLineChart = () => {
-//   Highcharts.chart('line-chart-container', {
-//
-//     title: {
-//       text: 'Solar Employment Growth by Sector, 2010-2016'
-//     },
-//
-//     subtitle: {
-//       text: 'Source: thesolarfoundation.com'
-//     },
-//
-//     yAxis: {
-//       title: {
-//         text: 'Number of Employees'
-//       }
-//     },
-//     legend: {
-//       layout: 'vertical',
-//       align: 'right',
-//       verticalAlign: 'middle'
-//     },
-//
-//     plotOptions: {
-//       series: {
-//         label: {
-//           connectorAllowed: false
-//         },
-//         pointStart: 2010
-//       }
-//     },
-//
-//     series: [{
-//       name: 'Installation',
-//       data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-//     }, {
-//       name: 'Manufacturing',
-//       data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-//     }, {
-//       name: 'Sales & Distribution',
-//       data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-//     }, {
-//       name: 'Project Development',
-//       data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-//     }, {
-//       name: 'Other',
-//       data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-//     }],
-//
-//     responsive: {
-//       rules: [{
-//         condition: {
-//           maxWidth: 500
-//         },
-//         chartOptions: {
-//           legend: {
-//             layout: 'horizontal',
-//             align: 'center',
-//             verticalAlign: 'bottom'
-//           }
-//         }
-//       }]
-//     }
-//
-//   });
-// }
 
 
 // Bar charts
